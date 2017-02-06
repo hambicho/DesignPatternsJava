1d0e007d1719c49b507be88761541dd281ba3acd
Henry Ambicho Trujillo <henry.ambicho@mercadolibre.cl>
2017-02-06 00:20:42 -0300

    No es necesario que se declare como interfaz en la clase Principal a las variables equipo

--- DI1_Java/src/Principal.java
+++ DI1_Java/src/Principal.java
@@ -3,7 +3,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		IEquipo equipo = new ManchesterUnited(); //new Barcelona();
+//		IEquipo equipo = new ManchesterUnited(); //new Barcelona();
+//		ManchesterUnited equipo = new ManchesterUnited();
+		Barcelona equipo = new Barcelona();
 		Jugador messi = new Jugador(equipo);
 		messi.mostrarEquipo();
 	}
